7c2b2a977b6cbc51de5129a5abf9f75e2bca7660
Steve Scales <design@stevescales.com>
2017-01-03 21:59:39 -0500

    start js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var items = document.getElementsByClassName("student-item");
+
+var itemsPerPage = 10;
+
+// var pageContainers =
+
+for ( var i = 0; i < items.length; i += itemsPerPage ) {
+  console.log(i); 
+}
