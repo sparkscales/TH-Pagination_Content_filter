860e886947796500086a4e4f0ed949c7c67df752
Steven Scales <stevenscales@Stevens-MacBook-Pro.local>
2017-01-10 16:46:46 -0500

    started working throught everything

--- js/app.js
+++ js/app.js
@@ -1,9 +1,51 @@
-var items = document.getElementsByClassName("student-item");
+// var items = document.getElementsByClassName("student-item");
+//
+// var itemsPerPage = 10;
+//
+// // var pageContainers =
 
-var itemsPerPage = 10;
+// Create and add a search bar
+// Add pagination to the page based on the amount of students being displayed on search results
 
-// var pageContainers =
+// Get Students
+var $students = $('.student-item');
 
-for ( var i = 0; i < items.length; i += itemsPerPage ) {
-  console.log(i); 
+// Set the number of students per page
+var studentsPerPage = 10;
+
+var currentPage = 0;
+// Get the number of pages
+var pages = Math.ceil($students.length / studentsPerPage);
+var studentlist = breakUpStudents();
+var studentListContiner = $('.student-list');
+
+  $students.hide();
+// breakup students
+function breakUpStudents() {
+  var list = $students.slice();
+  var newStudentsArray = [];
+  while (list.length) {
+    newStudentsArray.push(list.splice(0,studentsPerPage));
+  }
+  console.log(newStudentsArray);
+  return newStudentsArray;
+
+}
+
+
+// access and print the html in an array
+
+function outputPages() {
+
+// loop over the items in the array containing the 5 arrays of html
+  for(var i = 0; i < studentlist.length; i++ ) {
+    console.log(i);
+    studentListContiner.append (studentlist[0][0]);
+
+
+    // display only the array with the matching current page
+    if( currentPage === i ) {
+        studentlist.show();
+    }
+  }
 }
