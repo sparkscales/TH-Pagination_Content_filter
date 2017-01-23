eb1050fb45b8b8a37481cfb5554d2f448b0bb89f
Steven Scales <stevenscales@Stevens-MacBook-Pro.local>
2017-01-23 16:26:30 -0500

    got the pagination to work now focusing on the search field and attempting filter

--- js/app.js
+++ js/app.js
@@ -15,54 +15,96 @@ var $students = $('.student-item');
 // Set the number of students per page
 var studentsPerPage = 10;
 
-var currentPage = 0;
+var currentPage = 1;
 // Get the number of pages
 var pages = Math.ceil($students.length / studentsPerPage);
 var studentlist = breakUpStudents();
 var studentListContainer = $('.student-list');
 
   $students.remove();
-// breakup students
+  outputPages(0);
+
+// Create a new array of students and split them up based on the number of students per page
 function breakUpStudents() {
   var list = $students.slice();
   var newStudentsArray = [];
   while (list.length) {
     newStudentsArray.push(list.splice(0,studentsPerPage));
   }
-  console.log(newStudentsArray);
+  // console.log(newStudentsArray);
   return newStudentsArray;
 }
+// Append pagination to the page
 
 $('.page').append($("<div class='pagination'><ul></ul></div>"));
+generatePagination();
 
-// genereate pagination links
 function generatePagination() {
+  $(".pagination li").remove();
   for (var i = 1; i <= pages; i++) {
-    $(".pagination").append($("<li><a>" + i + "</a></li>"));
+    $(".pagination").append($("<li><a >" + i + "</a></li>"));
   }
+// assign pagination to the array
 }
+
+// Toggle class and select differetn array of students based on pagination
+$('.pagination li a').on("click", function() {
+  $('.pagination li a').removeClass("active");
+  $(this).addClass("active");
+  for(var index = 0; index <= studentlist.length; index++ ) {
+    if(parseInt($(this).text()) == index + 1 ) {
+      outputPages(index);
+      console.log('output-fired');
+    }
+  }
+});
+
 
 // Create and append search
 $(".page-header").append($("<div class='student-search'><input placeholder='Search for students...'><button>Search</button></div>"));
 
+// On click of the submit button grab the value of the search field and filter out the content based on the value
+
+$('button').on("click", search);
 
 
+
+function search() {
+  var search = $('.student-search input');
+  var searchContent = search.val().toLowerCase();
+    $students.remove();
+  // grab the value of the search field and compare it against the array of students
+
+  // Append the students that match the earch string
+  //
+  // for(var i = 0; i < studentlist.length; i++) {
+  //   var details = $('.student-details h3').text();
+  //   if(details.indexOf(searchContent) != -1 ) {
+  //     studentListContainer.append(studentlist[i]);
+  //   }
+  //   // if(details.indexOf())
+  // }
+
+  for(var i = 0; i < studentlist.length; i++) {
+    var index = 0;
+      if(searchContent) {
+        index = 
+      }
+  }
+
+}
 
 // access and print the html in an array
 
-function outputPages() {
-
-// loop over the items in the array containing the 5 arrays of html
-
+function outputPages(lpage) {
+  // remove the content before outputting more students
+  $students.remove();
+  // Loop through the studentslist array
   for(var i = 0; i < studentlist.length; i++ ) {
     console.log(i);
-    // studentlist[i].show();
+    // conditional if the number equals the index loop and append the contents
+    if(lpage === i ) {
       studentListContainer.append(studentlist[i]);
-
-
-    // display only the array with the matching current page
-    // if( currentPage === i ) {
-    //     studentlist.show();
-    // }
+    }
   }
 }
